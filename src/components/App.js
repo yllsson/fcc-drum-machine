221de9293cc2cd37bd22f58dfc726f65139702b8
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-03-01 16:27:23 +0000

    create drumpad component

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main className='App' id='drum-machine'>
+      <h1 id='display'>Hello world</h1>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/DrumPad.js
@@ -0,0 +1,5 @@
+const DrumPad = () => {
+  return <div className='drum-pad'>drum-pad</div>;
+};
+
+export default DrumPad;
